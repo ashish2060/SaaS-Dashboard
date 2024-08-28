f688c90c0b181c95f6efbebf0a66a559eb683824
Ashish Rawat <ashish23rd@gmail.com>
2024-08-29 01:12:12 +0530

    again redeploying

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-import dbConnect from "./dbConnect.js";
 import userRouer from "./routes/user.router.js";
+import dbConnect from "./utils/dbConnect.js";
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
@@ -9,29 +9,30 @@ import { v2 as cloudinary } from "cloudinary";
 import fileUpload from "express-fileupload";
 dotenv.config();
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://saas-dashboard-client.vercel.app"],
+  })
+);
+app.use(cookieParser());
+app.use("/", userRouer);
+app.use(fileUpload());
+
 app.listen(process.env.PORT, () => {
   console.log("app is listening at port 4000");
 });
+
+// database
+dbConnect();
 
 // test route
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Test Route" });
 });
-app.use(express.json());
-app.use(
-  cors({
-    origin: "https://saas-dashboard-client.vercel.app",
-  })
-);
-app.use(cookieParser());
-app.use("/user", userRouer);
-app.use(fileUpload());
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
-
-// database
-dbConnect();
--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const dbConnect = () => {
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URL)
     .then(() => {
       console.log("DB connected");
     })
