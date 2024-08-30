068f2a4a839a25f22a86c80742a84ee91d91c71d
Ashish Rawat <ashish23rd@gmail.com>
2024-08-31 03:40:52 +0530

    updating sendmail

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -24,16 +24,16 @@ export const sendMail = async (options) => {
     // const __dirname = path.dirname(__filename);
 
     // const templatePath = path.join(__dirname, "../mails/", template);
-    const templatePath = path.join(process.cwd(), `/mails/${template}`);
+    // const templatePath = path.join(process.cwd(), `/mails/${template}`);
 
     // render the email template with ejs
-    const html = await ejs.renderFile(templatePath, data);
+    // const html = await ejs.renderFile(templatePath, data);
 
     const mailOptions = {
       from: process.env.SMTP_MAIL,
       to: email,
       subject,
-      html: html,
+      html: `<b>Hello, <strong>${data.user}</strong>, Your OTP is:\n<b>${data.activationCode}</b></p>`,
     };
 
     await transpoter.sendMail(mailOptions);
